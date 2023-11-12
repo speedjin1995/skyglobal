2d3e5ee46b14ca244d43bb202995e2a4e36a4a6f
speedjin1995 <speedjin1995@gmail.com>
2023-11-12 14:01:36 +0800

    change insert Purchases

--- php/insertPurchase.php
+++ php/insertPurchase.php
@@ -31,60 +31,90 @@ if(isset($_POST['inputJobNo'], $_POST['inputDate'], $_POST['purchaseId'], $_POST
             $message[] = array( 
                 'purchaseId' => $purchaseId[$i],
                 'itemName' => $itemName[$i],
-                'itemPrice' => $itemPrice[$i]
+                'itemPrice' => $itemPrice[$i],
+                'images' => array()
             );
             $total+= $itemPrice[$i];
         }
     }
 
-    //insert sql
-    if ($insert_stmt = $db->prepare("INSERT INTO purchases (jobNo , date, items, total, created_datetime) VALUES (?, ?, ?, ?, ?)")) {
+    $select_stmt = $db->prepare("SELECT id, items, total FROM purchases WHERE jobNo = ?");
+    $select_stmt->bind_param('s', $inputJobNo);
+    $select_stmt->execute();
+    $result = $select_stmt->get_result();
+    $purchasesData = $result->fetch_assoc();
+	$claimList = array();
+    
+    if (empty($purchasesData)) {
+        if ($insert_stmt = $db->prepare("INSERT INTO purchases (jobNo , date, items, total, created_datetime) VALUES (?, ?, ?, ?, ?)")) {
+            $msg = json_encode($message);
+            $insert_stmt->bind_param('sssis', $inputJobNo, $inputDate, $msg, $total, $today);
+    
+            if (! $insert_stmt->execute()) {
+                echo json_encode(
+                    array(
+                        "status"=> "failed", 
+                        "message"=> $insert_stmt->error 
+                    )
+                );
+            }
+            else{
+                $purchase_id = $insert_stmt->insert_id;;
+                $insert_stmt->close();
+                $name = $_SESSION['name'];
+                $userId = $_SESSION['userID'];
+                $today = date("Y-m-d H:i:s");
+                
+                $action = "User : ".$name." Add Purchase id : ".$purchase_id." in customers table!";
+                
+                if ($log_insert_stmt = $db->prepare("INSERT INTO log (userId, userName, created_dateTime, action) VALUES (?,?,?,?)")) {
+                    $log_insert_stmt->bind_param('ssss', $userId, $name, $today, $action);
+                            
+                    if (! $log_insert_stmt->execute()) {
+                        /*echo json_encode(
+                            array(
+                                "status"=> "failed", 
+                                "message"=> $log_insert_stmt->error 
+                            )
+                        );*/
+                    }
+                }
+    
+              echo json_encode(
+                array(
+                    "status"=> "success", 
+                    "message"=> "Inserted Successfully!" 
+                )
+              );
+            }
+            
+            //$insert_stmt->close();
+            $db->close();
+            
+        }
+    } 
+	else {
+		$id = $purchasesData['id'];
         $msg = json_encode($message);
-        $insert_stmt->bind_param('sssis', $inputJobNo, $inputDate, $msg, $total, $today);
+        $update_stmt = $db->prepare("UPDATE purchases SET items=?, total=? WHERE id=?");
+        $update_stmt->bind_param('sss', $msg, $total, $id);
 
-        if (! $insert_stmt->execute()) {
+        if ($update_stmt->execute()) {
             echo json_encode(
                 array(
-                    "status"=> "failed", 
-                    "message"=> $insert_stmt->error 
+                    "status" => "success",
+                    "message" => "Updated Successfully!!"
+                )
+            );
+        } else {
+            echo json_encode(
+                array(
+                    "status" => "failed",
+                    "message" => $update_stmt->error
                 )
             );
         }
-        else{
-            $purchase_id = $insert_stmt->insert_id;;
-            $insert_stmt->close();
-            $name = $_SESSION['name'];
-            $userId = $_SESSION['userID'];
-            $today = date("Y-m-d H:i:s");
-            
-            $action = "User : ".$name." Add Purchase id : ".$purchase_id." in customers table!";
-            
-            if ($log_insert_stmt = $db->prepare("INSERT INTO log (userId, userName, created_dateTime, action) VALUES (?,?,?,?)")) {
-                $log_insert_stmt->bind_param('ssss', $userId, $name, $today, $action);
-                        
-                if (! $log_insert_stmt->execute()) {
-                    /*echo json_encode(
-                        array(
-                            "status"=> "failed", 
-                            "message"=> $log_insert_stmt->error 
-                        )
-                    );*/
-                }
-            }
-
-          echo json_encode(
-            array(
-                "status"=> "success", 
-                "message"=> "Inserted Successfully!" 
-            )
-          );
-        }
-        
-        //$insert_stmt->close();
-        $db->close();
-        
     }
-    
 }
 else{
     echo json_encode(
